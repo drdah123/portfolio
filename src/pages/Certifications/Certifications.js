3aa80cd6869d9ceccc3c27064fe036fa096f23c3
drdah123 <108929184+drdah123@users.noreply.github.com>
2023-04-18 13:22:26 +0300

    add url for certification

--- src/pages/Certifications/Certifications.js
+++ src/pages/Certifications/Certifications.js
@@ -24,20 +24,22 @@ const Certifications = () => {
       {certifications.map((item, i) => (
         <Col sm={6} md={4} lg={3} key={i} className="card-certification">
           <article>
-            <Card className="single-project">
-              <Card.Img src={item.img} />
-              <Card.ImgOverlay>
-                <Card.Title>
-                  <h4>{item.name}</h4>
-                </Card.Title>
-                <Card.Text className="projects-content">
-                  {item.degree}
-                </Card.Text>
-                <Card.Text className="projects-content mt-5">
-                  {item.date}
-                </Card.Text>
-              </Card.ImgOverlay>
-            </Card>
+            <a href={item.url} target="_blank" rel="noreferrer">
+              <Card className="single-project">
+                <Card.Img src={item.img} />
+                <Card.ImgOverlay>
+                  <Card.Title>
+                    <h4>{item.name}</h4>
+                  </Card.Title>
+                  <Card.Text className="projects-content">
+                    {item.degree}
+                  </Card.Text>
+                  <Card.Text className="projects-content mt-5">
+                    {item.date}
+                  </Card.Text>
+                </Card.ImgOverlay>
+              </Card>
+            </a>
           </article>
         </Col>
       ))}
